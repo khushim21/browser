7858b93056bc0b7ec2078ae606565ca2fa0da25a
khushim21 <83028354+khushim21@users.noreply.github.com>
2021-10-23 12:54:58 +0530

    initial commit

---
+++ Browser/Module_4_Excel_Clone/menu.js
@@ -0,0 +1,16 @@
+let fontSizeInput = document.querySelector(".font_size_input");
+
+// change
+fontSizeInput.addEventListener("change", function () {
+    let fontSize = fontSizeInput.value;
+    let address = addressInput.value;;
+    let ridcidObj = getRidCidFromAddress(address);
+    let tobeChangedCell = document.querySelector
+        (`.grid .cell[rId='${ridcidObj.rid}'][cId='${ridcidObj.cid}']`);
+    // change fontSize property
+    tobeChangedCell.style.fontSize = fontSize+"px";
+})
+// select -> fontFamily
+© 2021 GitHub, Inc.
+Terms
+Privacy
