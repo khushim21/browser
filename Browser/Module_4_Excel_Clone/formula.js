3db41965c6e9f0a884ecc0451a5257f89bfa1a79
khushim21 <83028354+khushim21@users.noreply.github.com>
2021-11-24 11:04:28 +0530

    initial commit

--- Browser/Module_4_Excel_Clone/formula.js
+++ Browser/Module_4_Excel_Clone/formula.js
@@ -1,18 +1,24 @@
 // cell -> formula remove /value set 
 for (let i = 0; i < AllGridCells.length; i++) {
-    AllGridCells[i].addEventListener("blur", function () {
+    AllGridCells[i].addEventListener("blur", function cellHelper(e) {
         let content = AllGridCells[i].textContent;
         let address = addressInput.value;
         let { rid, cid } = getRidCidFromAddress(address);
         let cellObject = db[rid][cid];
         // set self ui as well as children new values and ui
+        if (cellObject.value == content) {
+            return;
+        }
         if (cellObject.formula) {
-            removeFormula(address, formula);
+            removeFormula(address, cellObject.formula);
             cellObject.formula = "";
         }
         setUI(content, rid, cid);
     })
 }
+
+
+
 // formula bar
 // set Formula/update formula
 formulaInput.addEventListener("keydown", function (e) {
@@ -25,23 +31,22 @@ formulaInput.addEventListener("keydown", function (e) {
         let { rid, cid } = getRidCidFromAddress(addressOfTheCell);
         let cellObject = db[rid][cid];
         if (cellObject.formula != cFormula) {
-            removeFormula(addressOfTheCell, formula);
+            removeFormula(addressOfTheCell, cellObject.formula);
         }
         // console.log(cFormula);
         let value = evaluateFormula(cFormula);
-        console.log(value);
+        // console.log(value);
         setUI(value, rid, cid);
         cellObject.formula = cFormula;
         setFormula(addressOfTheCell, cFormula);
-
     }
 })
 function evaluateFormula(formula) {
-    console.log(formula);
+    // console.log(formula);
     // ( A1 + A2 ) -> ( 10 + 20 )
     let formulaEntities = formula.split(" ");
     // [(,A1,+,A2,)]
-    console.log(formulaEntities);
+    // console.log(formulaEntities);
     for (let i = 0; i < formulaEntities.length; i++) {
         let ascii = formulaEntities[i].charCodeAt(0);
         if (ascii >= 65 && ascii <= 90) {
@@ -53,7 +58,7 @@ function evaluateFormula(formula) {
             formula = formula.replace(formulaEntities[i], value);
         }
     }
-    console.log(formula);
+    // console.log(formula);
     // eval -> evaluate-> inbuilt 
     let result = eval(formula);
     return result;
@@ -71,15 +76,14 @@ function setUI(value, rid, cid) {
         let value = evaluateFormula(chCellObj.formula);
         setUI(value, chriciobj.rid, chriciobj.cid)
     }
-
 }
 //  to set a cell as children of a cell jispe depenedent 
 function setFormula(address, formula) {
-    console.log(formula);
+    // console.log(formula);
     // ( A1 + A2 ) -> ( 10 + 20 )
     let formulaEntities = formula.split(" ");
     // [(,A1,+,A2,)]
-    console.log(formulaEntities);
+    // console.log(formulaEntities);
     for (let i = 0; i < formulaEntities.length; i++) {
         let ascii = formulaEntities[i].charCodeAt(0);
         if (ascii >= 65 && ascii <= 90) {
@@ -96,11 +100,11 @@ function setFormula(address, formula) {
 }
 //  to set a cell as children of a cell jispe depenedent 
 function removeFormula(address, formula) {
-    console.log(formula);
+    // console.log(formula);
     // ( A1 + A2 ) -> ( 10 + 20 )
     let formulaEntities = formula.split(" ");
     // [(,A1,+,A2,)]
-    console.log(formulaEntities);
+    // console.log(formulaEntities);
     for (let i = 0; i < formulaEntities.length; i++) {
         let ascii = formulaEntities[i].charCodeAt(0);
         if (ascii >= 65 && ascii <= 90) {
@@ -109,9 +113,8 @@ function removeFormula(address, formula) {
             // db -> value
             let children = db[parentrcObj.rid][parentrcObj.cid].children;
             let idx = children.indexOf(address);
-            children.slice(idx, 1);
+            children.splice(idx, 1);
             // replace in formula
-
         }
     }
     console.log(formula);
